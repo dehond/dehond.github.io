f061d44d0150967440a443129dc40f477fee3a4b
Julius de Hond <dehond@mit.edu>
2022-09-05 21:11:40 -0400

    Add draft for bilayer post.

---
+++ assets/posts/bilayer/draw.js
@@ -0,0 +1,45 @@
+let height = 300;
+
+let svg = d3.select("#bilayer")
+  .attr("width", "100%")
+  .attr("height", height);
+
+let width = document.getElementById("bilayer").clientWidth;
+
+let xax = d3.scaleLinear()
+  .range([0, width])
+  .domain([-3, 3]);
+
+let yax = d3.scaleLinear()
+  .range([height, 0])
+  .domain([-20000, 0]);
+
+let a;
+d3.csv("/assets/posts/bilayer/output/coupled_potentials_tensor.csv", function(d) {
+    temp = {
+        x: Object.keys(d).map(parseFloat),
+        y: Object.values(d).map(parseFloat)
+    };
+    return temp.x.map( (d, i) => [d, temp.y[i]]);
+})
+  .then( function(d) {
+      a = d;
+      drawPotentials(a);
+  });
+
+
+let linegen = d3.line()
+    .x(d => xax(d[0]))
+    .y(d => yax(d[1]));
+
+function drawPotentials(lines) {
+    svg.append("g")
+      .attr("id", "curves")
+      .selectAll("path")
+      .data(lines)
+      .join("path")
+        .attr("d", d => linegen(d) )
+        .attr("stroke", "black")
+        .attr("stroke-width", 1)
+        .attr("fill", "none");
+}
